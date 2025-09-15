0aa0dc7715af533ae4ce7723c012e2403ea2343e
Ali <panahi.ali0606@gmail.com>
2025-09-15 14:13:27 +0300

    Add project files.

---
+++ BMI-kalkylator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BMI_kalkylator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
